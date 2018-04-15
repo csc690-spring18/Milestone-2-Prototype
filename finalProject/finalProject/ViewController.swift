5087d2d3ef71624b25b829dd3530a826a5a38fb2
boli <bojms45@gmail.com>
2018-04-15 01:07:43 -0700

    Create wire frame of login and register account

--- finalProject/finalProject/ViewController.swift
+++ finalProject/finalProject/ViewController.swift
@@ -12,6 +12,7 @@ class ViewController: UIViewController {
     
     @IBOutlet weak var menuView: UIView!
     @IBOutlet weak var leadingConstraint: NSLayoutConstraint!
+    
     var menuShowing = false
     
     override func viewDidLoad() {
@@ -21,11 +22,6 @@ class ViewController: UIViewController {
         menuView.layer.shadowRadius = 6 // shadow of left margin of menu bar
         
     }
-
-//    override func didReceiveMemoryWarning() {
-//        super.didReceiveMemoryWarning()
-//        // Dispose of any resources that can be recreated.
-//    }
 
     @IBAction func menuButton(_ sender: Any) {
         if ( menuShowing ) {
@@ -44,6 +40,7 @@ class ViewController: UIViewController {
         
         menuShowing = !menuShowing
     }
+    
     
 }
 
