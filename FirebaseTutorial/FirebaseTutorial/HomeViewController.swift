1881ec6d82267fcc34be48388762954302d67235
boli <bojms45@gmail.com>
2018-04-15 17:39:26 -0700

    Firebase API allow user to sign up, login, and reset password

---
+++ FirebaseTutorial/FirebaseTutorial/HomeViewController.swift
@@ -0,0 +1,38 @@
+//
+//  HomeViewController.swift
+//  FirebaseTutorial
+//
+//  Created by James Dacombe on 16/11/2016.
+//  Copyright © 2016 AppCoda. All rights reserved.
+//
+
+import UIKit
+import Firebase
+
+class HomeViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    @IBAction func logOutAction(sender: AnyObject) {
+        if Auth.auth().currentUser != nil {
+            do {
+                try Auth.auth().signOut()
+                let vc = UIStoryboard(name: "Main", bundle: nil).instantiateViewController(withIdentifier: "Login")
+                present(vc, animated: true, completion: nil)
+                
+            } catch let error as NSError {
+                print(error.localizedDescription)
+            }
+        }
+    }
+}
